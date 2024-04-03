6a394c04f106d2d8f012a55409263db4b0f09940
timbakkenes <tim.bakkenes@icloud.com>
2024-04-03 17:52:49 +0200

    changed name

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-function App() {
+const MapComponent = () => {
+  const mapStyles = {
+    height: "100vh",
+    width: "100%"
+  };
+
+  const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+  const defaultCenter = {
+    lat: 7.2905715,
+    lng: 80.6337262
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoadScript
+      googleMapsApiKey={API_KEY}
+    >
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={10}
+        center={defaultCenter}
+      >
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
   );
-}
+};
 
-export default App;
+export default MapComponent;
+
