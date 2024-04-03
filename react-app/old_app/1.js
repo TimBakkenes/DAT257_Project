6b98bebe0da172a9d287a772e5ba66e3df404d66
timbakkenes <tim.bakkenes@icloud.com>
2024-04-03 17:52:14 +0200

    hide_key

---
+++ react-app/old_app/1.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+// AIzaSyDqcwYMC3uRoHFK3RmqwFEFhwh0kChm0_Q
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello my name is Anton</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
