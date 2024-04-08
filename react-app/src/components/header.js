58872f820a062dcea215770749f3832de0e2f36b
ViktoriaAndersson <vianders@chalmers.se>
2024-04-08 15:20:23 +0200

    header

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import ".//css/Header.css"
+import "./css/Header.css"; // make sure the path is correct
 
-
-export function Header({ toggleFavorites , navigateLoginPage, navigateAboutPage}) {
+export function Header({ navigateHome, toggleFavorites, navigateFactPage, navigateProfilePage }) {
     return (
-      <div className="fixed-header">
-        {/* Här kan man lägga till och ta bort knappar från headern */}
+        <div className="fixed-header">
+          <button
+                className="header-button home-button"
+                onClick={navigateHome}
+                aria-label="Home" // Accessibility label
+            />
+            <button
+                className="header-button favorites-button"
+                onClick={toggleFavorites}
+                aria-label="Favorites" // Accessibility label
+            />
+            <button
+                className="header-button fact-button"
+                onClick={navigateFactPage}
+                aria-label="Fact" // Accessibility label
+            />
+            <button
+                className="header-button login-button"
+                onClick={navigateProfilePage}
+                aria-label="Login" // Accessibility label
+            />
+        </div>
 
-        <button classname="leftbutton" onClick={toggleFavorites}>
-            Favorites
-          </button>
-        
-        
-        <button onClick={navigateLoginPage} >
-            Login
-        </button>
-
-        <button onClick={navigateAboutPage} >
-            About
-        </button>
-        
-          
-     
-      </div>
     );
-  }
+}
