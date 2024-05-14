88b3e4f8c4dbba6bfa10817087127013577f1eeb
AntonJohansson02 <76776791+AntonJohansson02@users.noreply.github.com>
2024-05-14 10:32:15 +0200

    works

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,30 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from postgres_connection import DataBaseManager
-from contextlib import asynccontextmanager
+import postgres_connection as dc
+from postgres_connection import * 
+from pydantic import BaseModel
 
 
-dc = DataBaseManager()
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-   print("Connecting ... ")
-   dc.get_conn()
-   dc.get_cursor()
-   print("Connected")
-   yield
-   print("Disconnecting ... ")
-   dc.disconnect()
-   print("Disconnected")
+app = FastAPI()
 
-app = FastAPI(lifespan=lifespan)
+class FavouriteData(BaseModel):
+    user: str
+    store: str
 
-app.add_middleware(
-   CORSMiddleware,
-   allow_origins=['*'],
-   allow_credentials=True,
-   allow_methods=['*'],
-   allow_headers=['*']
-)
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#    print("Connecting ... ")
+#    dc.database_context()
+#    print("Connected")
+#    yield
+#    print("Disconnecting ... ")
+#    dc.disconnect()
+#    print("Disconnected")
+
+# app = FastAPI(lifespan=lifespan)
+
+# app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=['*'],
+#    allow_credentials=True,
+#    allow_methods=['*'],
+#    allow_headers=['*']
+# )
 
 
 @app.get("/")
@@ -32,32 +37,32 @@ def read_root():
    return {"message": "Hello, world!"}
 
 
-# Users
-@app.post("/api/post/add_user")
-async def add_user(id, user, bio):
-   return dc.add_user(id, user, bio)
+# # Users
+# @app.post("/api/post/add_user")
+# async def add_user(id, user, bio):
+#    return dc.add_user(id, user, bio)
 
-@app.get("/api/get/get_users")
-async def get_users():
-   return dc.get_users()
+# @app.get("/api/get/get_users")
+# async def get_users():
+#    return dc.get_users()
 
-@app.post("/api/post/remove_user")
-async def remove_user(id, user):
-   return dc.remove_user(id, user)
+# @app.post("/api/post/remove_user")
+# async def remove_user(id, user):
+#    return dc.remove_user(id, user)
 
 
-# Stores
-@app.get("/api/get/get_stores")
-async def get_stores():
-   return dc.get_favourites()
+# # Stores
+# @app.get("/api/get/get_stores")
+# async def get_stores():
+#    return dc.get_favourites()
 
-@app.post("/api/post/add_store")
-async def add_store(id, owner, name, lat, long):
-   return dc.add_store(id, owner, name, lat, long)
+# @app.post("/api/post/add_store")
+# async def add_store(id, owner, name, lat, long):
+#    return dc.add_store(id, owner, name, lat, long)
 
-@app.post("/api/post/remove_store")
-async def remove_store(id: str):
-   return dc.remove_store(id)
+# @app.post("/api/post/remove_store")
+# async def remove_store(id: str):
+#    return dc.remove_store(id)
 
 
 # Favourites
@@ -66,30 +71,36 @@ async def get_favourites(user: str):
    return dc.get_favourites(user)
 
 @app.post("/api/post/add_favourite")
-async def add_favourite(user: str, store: str):
-   return dc.add_favourite(user, store)
+async def add_favourite(favourite_data: favourite_model):
+    try:
+        dc.add_favourite(favourite_data)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/api/post/remove_favourite")
-async def remove_favourite(user: str, store: str):
-   return dc.remove_favourite(user, store)
+async def remove_favourite(favourite_data: favourite_model):
+   try:
+        return dc.remove_favourite(favourite_data)
+   except Exception as e:
+      raise HTTPException(status_code=400, detail=str(e))
+   
 
+# # Ratings
+# @app.get("/api/get/get_ratings")
+# async def get_rating(user: str, store:str):
+#    return dc.get_ratings(user, store)
 
-# Ratings
-@app.get("/api/get/get_ratings")
-async def get_rating(user: str, store:str):
-   return dc.get_ratings(user, store)
+# @app.get("/api/get/get_all_stores_ratings")
+# async def get_all_stores_ratings():
+#    return dc.get_all_stores_rating()
 
-@app.get("/api/get/get_all_stores_ratings")
-async def get_all_stores_ratings():
-   return dc.get_all_stores_rating()
+# @app.post("/api/post/add_rating")
+# async def add_rating(user: str, store: str, rating:int):
+#    return dc.add_rating(user, store, rating)
 
-@app.post("/api/post/add_rating")
-async def add_rating(user: str, store: str, rating:int):
-   return dc.add_rating(user, store, rating)
-
-@app.post("/api/post/remove_rating")
-async def remove_rating(user: str, store: str):
-   return dc.add_rating(user, store)
+# @app.post("/api/post/remove_rating")
+# async def remove_rating(user: str, store: str):
+#    return dc.add_rating(user, store)
 
 
 
--- backend/api/postgres_connection.py
+++ backend/api/postgres_connection.py
@@ -1,120 +1,147 @@
 import psycopg2
-
-class DatabaseConnection():
-    
-    @staticmethod
-    def get_connection():
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="postgres", port="5432") 
-        return conn
-    
-    @staticmethod
-    def get_cursor(conn):
-        return conn.cursor()
-    
-
-class DataBaseManager():
-    def __init__(self):
-        self.conn = None
-        self.cur = None
-    
-    def get_conn(self):
-        self.conn = DatabaseConnection.get_connection()
-    
-    def get_cursor(self):
-        self.cur = DatabaseConnection.get_cursor(self.conn)
-    
-    def disconnect(self):
-        self.conn.close()
-        self.cur.close()
+from pydantic import BaseModel
+from contextlib import contextmanager
 
 
-    def get_users(self):
-        query = """SELECT * FROM Users;"""
-        self.cur.execute(query)
-        value = self.cur.fetchall()
+@contextmanager
+def database_context():
+    conn = psycopg2.connect(host="localhost", dbname="agil", user="postgres", password="0", port="5432")
+    cur = conn.cursor()
+    try:
+        yield cur, conn
+    finally:
+        cur.close()
+        conn.close()
+
+# Perform database operations
+
+
+# class DatabaseConnection():
+    
+#     @staticmethod
+#     def get_connection():
+#         conn = psycopg2.connect(host="localhost", dbname="agil", user="postgres", password="0", port="5432") 
+#         return conn
+    
+#     @staticmethod
+#     def get_cursor(conn):
+#         return conn.cursor()
+    
+
+# class DataBaseManager():
+#     def __init__(self):
+#         self.conn = None
+#         self.cur = None
+
+#     def get_conn(self):
+#         self.conn = DatabaseConnection.get_connection()
+    
+#     def get_cursor(self):
+#         self.cur = DatabaseConnection.get_cursor(self.conn)
+    
+#     def disconnect(self):
+#         self.conn.close()
+#         self.cur.close()
+#     con_obj = DatabaseConnection().get_connection()
+#     cur_obj = DatabaseConnection().get_cursor(con_obj)
+
+##############################################
+
+# def get_users():
+#     query = """SELECT * FROM Users;"""
+#     cur.execute(query)
+#     value = cur.fetchall()
+#     return value
+
+# def add_user(id, user, bio):
+#     query = "INSERT INTO Users (id, username, bio) VALUES (%s, %s, %s)"
+#     val = (id, user, bio) 
+#     cur.execute(query, val)
+#     conn.commit()
+#     return "Success"
+
+# def add_stores(id, owner, name, lat, long):
+#     query = "INSERT INTO Stores (id, owner, name latitude, longitude) VALUES (%s, %s, %s, %s, %s)"
+#     val = (id, owner, name, lat, long) 
+#     cur.execute(query, val)
+#     conn.commit()
+#     return "Success"
+
+def get_favourites(user):
+    query = "SELECT store FROM Favourites WHERE user_id = %s"
+    val = (user,)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        value = cur.fetchall()
         return value
-    
-    def add_user(self, id, user, bio):
-        query = "INSERT INTO Users (id, username, bio) VALUES (%s, %s, %s)"
-        val = (id, user, bio) 
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
-    
-    def add_stores(self, id, owner, name, lat, long):
-        query = "INSERT INTO Stores (id, owner, name latitude, longitude) VALUES (%s, %s, %s, %s, %s)"
-        val = (id, owner, name, lat, long) 
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
-    
-    def get_favourites(self, user):
-        query = "SELECT store FROM Favourites WHERE user_id = %s"
-        val = (user,)
-        self.cur.execute(query, val)
-        result = self.cur.fetchall()
-        return result
-    
-    def add_favourite(self, user, store):
-        query = "INSERT INTO Favourites (user_id, store) VALUES (%s, %s);"
-        val = (user, store)
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
-    
-    def remove_favourite(self, user, store):
-        query = "DELETE FROM Favourites WHERE (user_id = %s AND stores = %s);"
-        val = (user, store)
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
-    
-    def remove_user(self, id):
-        query = "DELETE FROM Users WHERE (id = %s);"
-        val = (id,)
-        self.cur.execute(query, val)
-        self.conn.commit()
+
+
+class favourite_model(BaseModel):
+    user: str
+    store: str
+
+def add_favourite(fm: favourite_model):
+    query = "INSERT INTO Favourites (user_id, store) VALUES (%s, %s);"
+    val = (fm.user, fm.store)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
         return "Success"
 
-    def get_stores(self):
-        query = "SELECT * FROM Stores;"
-        self.cur.execute(query)
-        result = self.cur.fetchall()
+def remove_favourite(fm: favourite_model):
+    query = "DELETE FROM Favourites WHERE (user_id = %s AND store = %s);"
+    val = (fm.user, fm.store)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
         return "Success"
 
-    def remove_stores(self, id):
-        query = "DELETE FROM Stores WHERE (id = %s);"
-        val = (id,)
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
-    
-    def get_ratings(self, user, store):
-        query = "SELECT store FROM Ratings WHERE user_id = %s, store = %s"
-        val = (user,store)
-        self.cur.execute(query, val)
-        result = self.cur.fetchall()
-        return result
-    
-    def get_all_stores_rating(self):
-        query = "SELECT * FROM Agg_Ratings;"
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-        return result
+# def remove_user(id):
+#     query = "DELETE FROM Users WHERE (id = %s);"
+#     val = (id,)
+#     cur.execute(query, val)
+#     conn.commit()
+#     return "Success"
 
-    def add_rating(self, user, store, rating):
-        query = "INSERT INTO Ratings (user_id, store, rating) VALUES (%s, %s, %s);"
-        val = (user, store, rating)
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
+# def get_stores():
+#     query = "SELECT * FROM Stores;"
+#     cur.execute(query)
+#     result = cur.fetchall()
+#     return "Success"
 
-    def remove_rating(self, user, store):
-        query = "DELETE FROM Ratings WHERE (user_id = %s, store = %s)"
-        val = (user, store)
-        self.cur.execute(query, val)
-        self.conn.commit()
-        return "Success"
+# def remove_stores(id):
+#     query = "DELETE FROM Stores WHERE (id = %s);"
+#     val = (id,)
+#     cur.execute(query, val)
+#     conn.commit()
+#     return "Success"
+
+# def get_ratings(user, store):
+#     query = "SELECT store FROM Ratings WHERE user_id = %s, store = %s"
+#     val = (user,store)
+#     cur.execute(query, val)
+#     result = cur.fetchall()
+#     return result
+
+# def get_all_stores_rating():
+#     query = "SELECT * FROM Agg_Ratings;"
+#     cur.execute(query)
+#     result = cur.fetchall()
+#     return result
+
+# def add_rating( user, store, rating):
+#     query = "INSERT INTO Ratings (user_id, store, rating) VALUES (%s, %s, %s);"
+#     val = (user, store, rating)
+#     cur.execute(query, val)
+#     conn.commit()
+#     return "Success"
+
+# def remove_rating(user, store):
+#     query = "DELETE FROM Ratings WHERE (user_id = %s, store = %s)"
+#     val = (user, store)
+#     cur.execute(query, val)
+#     conn.commit()
+#     return "Success"
 
 
         
