da0926167874008b75ab88839b8465ef05aadb76
AntonJohansson02 <76776791+AntonJohansson02@users.noreply.github.com>
2024-05-14 11:04:26 +0200

    rewrote funcitons in postgress_connection

--- backend/api/postgres_connection.py
+++ backend/api/postgres_connection.py
@@ -14,25 +14,31 @@ def database_context():
         conn.close()
 
 
-# def get_users():
-#     query = """SELECT * FROM Users;"""
-#     cur.execute(query)
-#     value = cur.fetchall()
-#     return value
+# Users
+def get_users():
+    query = """SELECT * FROM Users;"""
+    with database_context() as (cur, conn):
+        cur.execute(query)
+        value = cur.fetchall()
+        return value
+    
 
-# def add_user(id, user, bio):
-#     query = "INSERT INTO Users (id, username, bio) VALUES (%s, %s, %s)"
-#     val = (id, user, bio) 
-#     cur.execute(query, val)
-#     conn.commit()
-#     return "Success"
+def add_user(id, user, bio):
+    query = "INSERT INTO Users (id, username, bio) VALUES (%s, %s, %s)"
+    val = (id, user, bio) 
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
+        return "Success"
+    
+def remove_user(id):
+    query = "DELETE FROM Users WHERE (id = %s);"
+    val = (id,)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
+        return "Success"
 
-# def add_stores(id, owner, name, lat, long):
-#     query = "INSERT INTO Stores (id, owner, name latitude, longitude) VALUES (%s, %s, %s, %s, %s)"
-#     val = (id, owner, name, lat, long) 
-#     cur.execute(query, val)
-#     conn.commit()
-#     return "Success"
 
 
 # favourites 
@@ -61,52 +67,63 @@ def remove_favourite(user:str, store:str):
         conn.commit()
         return "Success"
 
-# def remove_user(id):
-#     query = "DELETE FROM Users WHERE (id = %s);"
-#     val = (id,)
-#     cur.execute(query, val)
-#     conn.commit()
-#     return "Success"
 
-# def get_stores():
-#     query = "SELECT * FROM Stores;"
-#     cur.execute(query)
-#     result = cur.fetchall()
-#     return "Success"
+# Stores
+def get_stores():
+    query = "SELECT * FROM Stores;"
+    with database_context() as (cur, conn):
+        cur.execute(query)
+        result = cur.fetchall()
+        return result
 
-# def remove_stores(id):
-#     query = "DELETE FROM Stores WHERE (id = %s);"
-#     val = (id,)
-#     cur.execute(query, val)
-#     conn.commit()
-#     return "Success"
+def add_stores(id, owner, name, lat, long):
+    query = "INSERT INTO Stores (id, owner, name, latitude, longitude) VALUES (%s, %s, %s, %s, %s)"
+    val = (id, owner, name, lat, long) 
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
+        return "Success"
 
-# def get_ratings(user, store):
-#     query = "SELECT store FROM Ratings WHERE user_id = %s, store = %s"
-#     val = (user,store)
-#     cur.execute(query, val)
-#     result = cur.fetchall()
-#     return result
+def remove_stores(id):
+    query = "DELETE FROM Stores WHERE (id = %s);"
+    val = (id,)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
+        return "Success"
 
-# def get_all_stores_rating():
-#     query = "SELECT * FROM Agg_Ratings;"
-#     cur.execute(query)
-#     result = cur.fetchall()
-#     return result
 
-# def add_rating( user, store, rating):
-#     query = "INSERT INTO Ratings (user_id, store, rating) VALUES (%s, %s, %s);"
-#     val = (user, store, rating)
-#     cur.execute(query, val)
-#     conn.commit()
-#     return "Success"
+# Ratings
+def get_ratings(user, store):
+    query = "SELECT store FROM Ratings WHERE user_id = %s AND store = %s"
+    val = (user, store)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        result = cur.fetchall()
+        return result
 
-# def remove_rating(user, store):
-#     query = "DELETE FROM Ratings WHERE (user_id = %s, store = %s)"
-#     val = (user, store)
-#     cur.execute(query, val)
-#     conn.commit()
-#     return "Success"
+def get_all_stores_rating():
+    query = "SELECT * FROM Agg_Ratings;"
+    with database_context() as (cur, conn):
+        cur.execute(query)
+        result = cur.fetchall()
+        return result
+
+def add_rating(user, store, rating):
+    query = "INSERT INTO Ratings (user_id, store, rating) VALUES (%s, %s, %s);"
+    val = (user, store, rating)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
+        return "Success"
+
+def remove_rating(user, store):
+    query = "DELETE FROM Ratings WHERE (user_id = %s AND store = %s)"
+    val = (user, store)
+    with database_context() as (cur, conn):
+        cur.execute(query, val)
+        conn.commit()
+        return "Success"
 
 
         
