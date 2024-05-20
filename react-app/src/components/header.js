7ff01ba73d2702c562e05245fd1c6df21f56282a
timbakkenes <tim.bakkenes@icloud.com>
2024-05-20 09:29:28 +0200

    Added logout button

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import "./css/Header.css"; // make sure the path is correct
+import { useNavigate } from 'react-router-dom'
 
 export function Header({toggleFavorites, toggleFactpage, navigateProfilePage, toggleContact }) {
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        navigate("/")
+        window.location.reload()
+    }
+
     return (
 
         <div className="fixed-header">
-          {/* <button
-                className="header-button map-button"
-                onClick={toggleMap}
-                aria-label="Map" // Accessibility label
-            /> */}
+          
             <button
                 className="header-button favorites-button"
                 onClick={toggleFavorites}
-                aria-label="Favorites" // Accessibility label
+                aria-label="Favorites" 
             />
             <button
                 className="header-button fact-button"
                 onClick={toggleFactpage}
-                aria-label="Fact" // Accessibility label
+                aria-label="Fact"
             />
             <button
                 className="header-button profile-button"
                 onClick={navigateProfilePage}
-                aria-label="Profile" // Accessibility label
+                aria-label="Profile"
             />
             <button
                 className="header-button contact-button"
                 onClick={toggleContact}
-                aria-label="Contact" // Accessibility label
+                aria-label="Contact"
             />
+
+            <button
+                className="log-out"
+                onClick={logout}
+            >Log Out</button>
+
         </div>
 
     );
